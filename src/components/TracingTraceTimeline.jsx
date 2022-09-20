b5cb862c076814b0af36595252321c0aa0b0db6f
alexey.belous <alexey.belous@hygge.software>
2022-09-20 22:49:37 +0300

    formatted trace-timeline__item__time

--- src/components/TracingTraceTimeline.jsx
+++ src/components/TracingTraceTimeline.jsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import "./../styles/TracingTraceTimeline.scss";
 
+const padTo2Digits = (num) => {
+    return num.toString().padStart(2, '0');
+}
+
+const formatDate = (date) => {
+    return (
+        [
+            date.getFullYear(),
+            padTo2Digits(date.getMonth() + 1),
+            padTo2Digits(date.getDate()),
+        ].join('-') +
+        ' ' +
+        [
+            padTo2Digits(date.getHours()),
+            padTo2Digits(date.getMinutes()),
+            padTo2Digits(date.getSeconds()),
+        ].join(':')
+    );
+}
+
 const TracingTraceTimeline = ({ steps }) => {
     return (
         <div className="trace-timeline">
@@ -11,7 +31,7 @@ const TracingTraceTimeline = ({ steps }) => {
                     >
                         <div className="trace-timeline__item__name">{ step.name }</div>
                         <div className="trace-timeline__item__dots" />
-                        <div className="trace-timeline__item__time">{ step.datetime }</div>
+                        <div className="trace-timeline__item__time">{ formatDate(step.datetime) }</div>
                     </div>
                 )
             )}
